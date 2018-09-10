165243e306203dab6c8cebccf1c79c67e649ad5e
Marcelo <marcelo@Marcelos-MacBook-Pro.local>
2018-09-09 23:35:07 -0300

    Prueba de commit en branch P3-Buscador

---
+++ app/src/main/java/ort/edu/org/pelisapp/p3_buscador.java
@@ -0,0 +1,13 @@
+package ort.edu.org.pelisapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class p3_buscador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p3_buscador);
+    }
+}
