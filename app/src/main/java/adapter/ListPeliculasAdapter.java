58fa7952e0d55f5b8be8f434175f7c64974e7b68
gastonandresgonzalez <gaston.gonzalez.1987@gmail.com>
2018-09-17 16:09:19 -0300

    Funcionando Primer Listado
    Funcionando Primer Listado

--- app/src/main/java/adapter/ListPeliculasAdapter.java
+++ app/src/main/java/adapter/ListPeliculasAdapter.java
@@ -39,7 +39,8 @@ public class ListPeliculasAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        View v= View.inflate(mContext, R.layout.item_peliculas, null );
+        View v= View.inflate(mContext, R.layout.item_peliculas , null );
+        TextView tv_pelicula_id = v.findViewById(R.id.tv_pelicula_id  );
         TextView tv_pelicula_titulo = v.findViewById(R.id.tv_pelicula_titulo  );
         TextView tv_pelicula_anio = v.findViewById(R.id.tv_pelicula_anio  );
         TextView tv_pelicula_url = v.findViewById(R.id.tv_pelicula_url  );
@@ -48,6 +49,16 @@ public class ListPeliculasAdapter extends BaseAdapter {
         TextView tv_pelicula_rating = v.findViewById(R.id.tv_pelicula_rating  );
         TextView tv_pelicula_director = v.findViewById(R.id.tv_pelicula_director  );
         TextView tv_pelicula_genero = v.findViewById(R.id.tv_pelicula_genero  );
+
+        tv_pelicula_id.setText(String.valueOf(mPeliculaList.get(position).getId ()));
+        tv_pelicula_titulo.setText(String.valueOf(mPeliculaList.get(position).getTitulo()));
+        tv_pelicula_anio.setText(String.valueOf(mPeliculaList.get(position).getAnio ()));
+        tv_pelicula_url.setText(String.valueOf(mPeliculaList.get(position).getUrl()));
+        tv_pelicula_descripcion.setText(String.valueOf(mPeliculaList.get(position).getDescripcion()));
+        tv_pelicula_duracion.setText(String.valueOf(mPeliculaList.get(position).getDuracion()));
+        tv_pelicula_rating.setText(String.valueOf(mPeliculaList.get(position).getRating()));
+        tv_pelicula_director.setText(String.valueOf(mPeliculaList.get(position).getDirector()));
+        tv_pelicula_genero.setText(String.valueOf(mPeliculaList.get(position).getGenero()));
 
     return v;
 
